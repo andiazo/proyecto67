fe4425e6e762c491652cb2d1df2f5b4c6c0f9000
Andres Diaz <andiazo@unal.edu.co>
2022-03-17 10:35:38 -0500

    first commit

---
+++ src/views/LandingView.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './LandingView.css';
+import logo from './logo.svg';
+import logoTPI from './logoTPI.png';
+import logoBAVARIA from './logoBAVARIA.png';
+
+class LandingView extends React.Component {
+    render() {
+        return(
+            <div className='landing'>
+                    <div className='title'>
+                        <h1>Proyecto 67</h1>
+                        <p>Revolucionando el consumo eléctrico en Bavaria... Esperanos!</p>
+                    </div>
+                    <div className='partners'>
+                        <h3>Patrocinadores</h3>
+                        <span>
+                            <img src={logoTPI} alt="logo TPI" />
+                            <img src={logoBAVARIA} alt="logo BAVARIA" />
+                        </span>
+                        <span>
+                            
+                        </span>
+                    </div>
+            </div>
+        )
+    }
+}
+
+export default LandingView;
