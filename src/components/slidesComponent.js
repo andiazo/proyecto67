399fd331e42ebe8aa78efc1b67b1cb8aef4f41e0
Andres Diaz <andiazo@unal.edu.co>
2022-04-06 16:53:18 -0500

    Update info

---
+++ src/components/slidesComponent.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class SlidesComponent extends Component {
+  render() {
+    return (
+      <div>
+          <iframe 
+            src="https://docs.google.com/presentation/d/e/2PACX-1vR9AwwJu-5fukdKMO04tj6X78uMjoluomWP5sbr_5RFj-3z-0PeLCQKIE4SHlBA8nNyf4K0rCJSN93g/embed?start=false&loop=false&delayms=3000" 
+            frameborder="0" width="1440" height="839" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>
+      </div>
+    )
+  }
+}
